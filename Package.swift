7f9852935cef67c7df71b8e74c55eea08cd9deca
deflating <matt.kennelly@me.com>
2025-06-23 02:53:59 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Neumorphic",
-            targets: ["Neumorphic"]),
+            name: "Neumorphic2",
+            targets: ["Neumorphic2"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Neumorphic",
+            name: "Neumorphic2",
             dependencies: []),
         .testTarget(
-            name: "NeumorphicTests",
+            name: "Neumorphic2Tests",
             dependencies: ["Neumorphic"]),
     ]
 )
